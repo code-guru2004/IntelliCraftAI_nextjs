60010511ff927abc89ef910804b2a2a5ac3acb25
Nayan Das <nayan010das@gmail.com>
2024-12-19 00:23:02 +0530

    Your commit message describing the changes

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -13,19 +13,24 @@ interface MongooseConnection{
 
 let cached: MongooseConnection = (global as any).mongoose
 
-if(!cached){
-    cached = (global as any).mongoose = { conn: null, promise: null };
+if(!cached) {
+  cached = (global as any).mongoose = { 
+    conn: null, promise: null 
+  }
 }
 
-export const connectToDatabase = async()=>{
-    if (cached.conn) {
-      return cached.conn;
-    }
-    
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URL , {dbName:"IntelliCraftAI" , bufferCommands:false});
+export const connectToDatabase = async () => {
+  if(cached.conn) return cached.conn;
 
-    cached.conn = await cached.promise;
+  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
 
-    return cached.conn
+  cached.promise = 
+    cached.promise || 
+    mongoose.connect(MONGODB_URL, { 
+      dbName: 'imaginify', bufferCommands: false 
+    })
 
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 }
