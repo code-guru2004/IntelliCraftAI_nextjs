3200a6437d066db7b18512e1a831ffaedc6d531c
Nayan Das <nayan010das@gmail.com>
2025-01-23 17:49:23 +0530

    route

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -79,7 +79,6 @@ export async function POST(req: Request) {
   if(eventType ==="user.updated"){
     const { id, image_url, first_name, last_name, username } = evt.data;
     const user = {
-      clerkId: id,
       username: username!,
       firstName: first_name,
       lastName: last_name||"",
