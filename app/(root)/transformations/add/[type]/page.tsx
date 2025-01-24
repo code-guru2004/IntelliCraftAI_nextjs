a51f73d34ed05d9b150f67254e8929de8d141080
Nayan Das <nayan010das@gmail.com>
2025-01-24 20:10:35 +0530

    m

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,44 +1,35 @@
-import Header from "@/components/shared/Header";
-import React from "react";
-import { transformationTypes } from "@/constants";
-import TransformationForm from "@/components/shared/TransformationForm";
+import Header from '@/components/shared/Header'
+import TransformationForm from '@/components/shared/TransformationForm';
+import { transformationTypes } from '@/constants'
+import { getUserById } from '@/lib/actions/user.action';
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation';
 
-import { getUserById } from "@/lib/actions/user.action";
-import { redirect } from "next/navigation";
-import { auth } from '@clerk/nextjs/server';
-
-interface SearchParamProps {
-  params: {
-    id: string;
-    type: TransformationTypeKey;
-  };
-}
-
-const AddTransformationTypePage: React.FC<SearchParamProps> = async ({ params: { type } }) => { 
-  // Note: The async keyword is moved to the component definition
-
-  const { userId } = await auth()
+const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
+  const { userId } =await auth();
   const transformation = transformationTypes[type];
-  if(!userId) redirect('/sign-in') 
-  const userDetails = await getUserById(userId);
+
+  if(!userId) redirect('/sign-in')
+
+  const user = await getUserById(userId);
 
   return (
     <>
       <Header 
-        title={transformationTypes[type].title} 
-        subtitle={transformationTypes[type].subTitle} 
+        title={transformation.title}
+        subtitle={transformation.subTitle}
       />
-
+    
       <section className="mt-10">
-        <TransformationForm
-          action="Add" 
-          userId={userDetails._id}
+        <TransformationForm 
+          action="Add"
+          userId={user._id}
           type={transformation.type as TransformationTypeKey}
-          creditBalance={userDetails.creditBalance}
+          creditBalance={user.creditBalance}
         />
       </section>
     </>
   )
-};
+}
 
-export default AddTransformationTypePage;
+export default AddTransformationTypePage
