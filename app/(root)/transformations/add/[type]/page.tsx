7f225dd02584b2aea4fa7e6fdbf3ad25e1f793fd
Nayan Das <nayan010das@gmail.com>
2025-01-24 20:25:54 +0530

    a

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -9,12 +9,7 @@ import { getUserById } from "@/lib/actions/user.action";
 import { redirect } from "next/navigation";
 import { auth } from '@clerk/nextjs/server'
 
-interface SearchParamProps {
-  params: {
-    id: string; 
-    type: TransformationTypeKey; 
-  };
-}
+
 const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
   const transformation = transformationTypes[type];
   const { userId } = await auth()
@@ -30,12 +25,12 @@ const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps)
       />
     
       <section className="mt-10">
-      {/* <TransformationForm
+      <TransformationForm
             action="Add" 
             userId={userDetails._id}
             type={transformation.type as TransformationTypeKey}
             creditBalance={userDetails.creditBalance}
-      /> */}
+      />
       </section> 
     </>
   )
