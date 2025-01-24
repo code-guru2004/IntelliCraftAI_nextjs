4e6ac05c28fe21b67c7b3e398cae96ddf383cfb7
Nayan Das <nayan010das@gmail.com>
2025-01-24 20:44:34 +0530

    a

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,41 +1,45 @@
-
 import Header from "@/components/shared/Header";
 import React from "react";
 import { transformationTypes } from "@/constants";
 import TransformationForm from "@/components/shared/TransformationForm";
 
-
 import { getUserById } from "@/lib/actions/user.action";
 import { redirect } from "next/navigation";
-import { auth } from '@clerk/nextjs/server'
+import { auth } from '@clerk/nextjs/server';
 
+interface SearchParamProps {
+  params: {
+    id: string;
+    type: TransformationTypeKey;
+  };
+}
 
+const AddTransformationTypePage: React.FC<SearchParamProps> = async ({ params }) => {
+  const { type } = params; 
+  const transformation = transformationTypes[type]; 
 
-export const AddTransformationTypePage: React.FC<SearchParamProps> = async ({ param }) => {
-  const transformation = transformationTypes[param?.type];
-  const { userId } = await auth()
-  
+  const { userId } = await auth(); 
+
   if(!userId) redirect('/sign-in') 
   const userDetails = await getUserById(userId);
 
   return (
     <>
       <Header 
-        title={transformation?.title}
-        subtitle={transformation?.subTitle}
+        title={transformation?.title} 
+        subtitle={transformation?.subTitle} 
       />
-    
+
       <section className="mt-10">
-      <TransformationForm
-            action="Add" 
-            userId={userDetails._id}
-            type={transformation?.type as TransformationTypeKey}
-            creditBalance={userDetails.creditBalance}
-      />
-      </section> 
+        <TransformationForm
+          action="Add" 
+          userId={userDetails._id}
+          type={transformation?.type as TransformationTypeKey}
+          creditBalance={userDetails.creditBalance}
+        />
+      </section>
     </>
   )
-}
+};
 
-// export default AddTransformationTypePage
-
+export default AddTransformationTypePage;
