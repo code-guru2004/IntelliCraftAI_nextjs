fa366d12ce0ed64b229f06eb6bab4d5ae49d3f9a
Nayan Das <nayan010das@gmail.com>
2025-01-24 19:41:53 +0530

    page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,9 +4,9 @@ import { navLinks } from "@/constants"
 import Image from "next/image"
 import Link from "next/link"
 
-const Home = async ({ searchParams }: SearchParamProps) => {
-  const page = Number(searchParams?.page) || 1;
-  const searchQuery = (searchParams?.query as string) || '';
+const Home = async () => {
+  // const page = Number(searchParams?.page) || 1;
+  // const searchQuery = (searchParams?.query as string) || '';
 
  
 
