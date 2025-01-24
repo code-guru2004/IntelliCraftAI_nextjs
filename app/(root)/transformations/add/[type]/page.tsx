9dd8404cbae8532acad31b4a0db5ebf3e6c4021a
Nayan Das <nayan010das@gmail.com>
2025-01-24 19:54:14 +0530

    a

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -9,23 +9,51 @@ import { getUserById } from "@/lib/actions/user.action";
 import { redirect } from "next/navigation";
 import { auth } from '@clerk/nextjs/server'
 
-async function AddTransformationTypePage({ params: { id , type } ,searchParams }: SearchParamProps) {
+// async function AddTransformationTypePage({ params: { type } , searchParams }: SearchParamProps) {
+//   const transformation = transformationTypes[type];
+//   const { userId } = await auth()
+  
+//   if(!userId) redirect('/sign-in')
+//   const userDetails = await getUserById(userId);
+//   return (
+//     <>
+//       <Header title={transformation.title} subtitle={transformation.subTitle} />
+//       <TransformationForm
+//             action="Add" 
+//             userId={userDetails._id}
+//             type={transformation.type as TransformationTypeKey}
+//             creditBalance={userDetails.creditBalance}
+//       />
+//     </>
+//   );
+// }
+
+// export default AddTransformationTypePage;
+
+const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
   const transformation = transformationTypes[type];
   const { userId } = await auth()
   
   if(!userId) redirect('/sign-in')
   const userDetails = await getUserById(userId);
+
   return (
     <>
-      <Header title={transformation.title} subtitle={transformation.subTitle} />
+      <Header 
+        title={transformation.title}
+        subtitle={transformation.subTitle}
+      />
+    
+      <section className="mt-10">
       <TransformationForm
             action="Add" 
             userId={userDetails._id}
             type={transformation.type as TransformationTypeKey}
             creditBalance={userDetails.creditBalance}
       />
+      </section>
     </>
-  );
+  )
 }
 
-export default AddTransformationTypePage;
+export default AddTransformationTypePage
